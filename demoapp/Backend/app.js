defbf9e99720710148a7c60ef30aca4ee820ddf0
hawa ebrahim <hawaebrahimhamid@gmail.com>
2026-03-30 00:52:54 +0800

    fix deploy

--- demoapp/Backend/app.js
+++ demoapp/Backend/app.js
@@ -20,6 +20,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // ✅ VERY IMPORTANT
 };
 // create the connection to the database
 const connection = mysql.createConnection(dbConfig);
